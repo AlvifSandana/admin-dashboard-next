ffa8deaecf52a8007bfbd76bc0e8dab30f2b8043
AlvifSandana <asm240800@gmail.com>
2023-01-27 20:54:33 +0700

    change to syncronous script

--- components/admin/scripts.js
+++ components/admin/scripts.js
@@ -3,20 +3,20 @@ import Script from "next/script";
 export default function Scripts(){
   return (
     <>
-      <Script defer src="/assets/modules/jquery.min.js" />
-      <Script defer src="/assets/modules/popper.js" />
-      <Script defer src="/assets/modules/tooltip.js" />
-      <Script defer src="/assets/modules/bootstrap/js/bootstrap.min.js" />
-      <Script defer src="/assets/modules/nicescroll/jquery.nicescroll.min.js" />
-      <Script defer src="/assets/modules/moment.min.js" />
-      <Script defer src="/assets/js/stisla.js" />
-      <Script defer src="/assets/modules/jquery.sparkline.min.js" />
-      <Script defer src="/assets/modules/chart.min.js" />
-      <Script defer src="/assets/modules/owlcarousel2/dist/owl.carousel.min.js" />
-      <Script defer src="/assets/modules/summernote/summernote-bs4.js" />
-      <Script defer src="/assets/modules/chocolat/dist/js/jquery.chocolat.min.js" />
-      <Script defer src="/assets/js/scripts.js" />
-      <Script defer src="/assets/js/custom.js" />
+      <script src="/assets/modules/jquery.min.js" />
+      <script src="/assets/modules/popper.js" />
+      <script src="/assets/modules/tooltip.js" />
+      <script src="/assets/modules/bootstrap/js/bootstrap.min.js" />
+      <script src="/assets/modules/nicescroll/jquery.nicescroll.min.js" />
+      <script src="/assets/modules/moment.min.js" />
+      <script src="/assets/js/stisla.js" />
+      <script src="/assets/modules/jquery.sparkline.min.js" />
+      <script src="/assets/modules/chart.min.js" />
+      <script src="/assets/modules/owlcarousel2/dist/owl.carousel.min.js" />
+      <script src="/assets/modules/summernote/summernote-bs4.js" />
+      <script src="/assets/modules/chocolat/dist/js/jquery.chocolat.min.js" />
+      <script src="/assets/js/scripts.js" />
+      <script src="/assets/js/custom.js" />
     </>
   )
 }
